7e8dc1c0756c3a7bf99823461e46bbadf934d041
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2020-12-18 10:35:43 +0100

    Agregar archivos de proyecto.

---
+++ Actividad2_Comida/BanderaConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Actividad2_Comida
+{
+    class BanderaConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value == "Mexicana")
+                return "Assets/mexico.png";
+            else if ((string)value == "Americana")
+                return "Assets/united_states.png";
+            else if ((string)value == "China")
+                return "Assets/china.png";
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Actividad2_Comida/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Actividad2_Comida
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Plato> listaPlatos;
+        public MainWindow()
+        {
+            InitializeComponent();
+            listaPlatos = Plato.GetSamples(@"E:/\DAM\2DAM\DINT\UT5\Imagenes\");
+            List<string> listaComida = new List<string>() { "Americana", "Mexicana", "China" };
+            listBox.DataContext = listaPlatos;
+            comboBox.ItemsSource = listaComida;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            stackpanel.DataContext = listBox.SelectedItem;
+            stackPanelComboBox.DataContext = listBox.SelectedItem;
+        }
+    }
+}
